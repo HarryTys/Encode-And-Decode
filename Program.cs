042f6ab6fe9335c92558e6efac10c84747ee9cd2
HarryTys <113461358+HarryTys@users.noreply.github.com>
2022-12-06 00:15:25 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Encode_Decode_Program
+{
+    class Globals
+    {
+        public static string encodeDecodeKey;
+        public static int encodeInteger;
+        public static string encodedString;
+        public static string decodedString;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Enter a string which you want encoded and decoded: "); string encodeString = Console.ReadLine();
+
+            for (int x = 0; x < encodeString.Length; x++)
+            {
+                int asciiValue = encodeString[x];
+                Globals.encodeInteger = rnd.Next(1, 9);
+                Globals.encodeDecodeKey = Globals.encodeDecodeKey + Globals.encodeInteger;
+                char encodedCharacter = (char)(asciiValue + Globals.encodeInteger);
+                Globals.encodedString = Globals.encodedString + encodedCharacter;
+                Console.WriteLine("ascii value of character " + x + " in the user inputted string.");
+                Console.WriteLine("encoded character " + x + " is " + encodedCharacter);
+            }
+
+            for (int x = 0; x < Globals.encodedString.Length; x++)
+            {
+                int asciiValue = Globals.encodedString[x];
+                char decodedCharacter = (char)(asciiValue - (int)Globals.encodeDecodeKey[x]);
+                Globals.decodedString = Globals.decodedString + decodedCharacter;
+                Console.WriteLine("ascii value of encoded character " + x + " equals " + asciiValue);
+                Console.WriteLine("Decoded character equals " + decodedCharacter);
+            }
+
+            Console.WriteLine("Encoded string: " + Globals.encodedString);
+            Console.WriteLine("Decoded string: " + Globals.decodedString);
+            Console.ReadLine();
+        }
+    }
+}
